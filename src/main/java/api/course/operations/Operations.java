89614e88f42c3f93133f8b11d88ee775bf4406ae
Jairo  Roldan <jairo.roldan@grupobabel.com>
2022-05-12 20:11:22 -0500

    cambio prueba pipeline github actions

--- src/main/java/api/course/operations/Operations.java
+++ src/main/java/api/course/operations/Operations.java
@@ -1,9 +1,13 @@
-package api.course.operations;
+ipackage api.course.operations;
 
 public class Operations {
 	
 	public int sum (int a, int b) {
 		return  a + b;
 	}
+
+        public int multiplicate (int a, int b) {
+                return a * b;
+        }
 
 }
